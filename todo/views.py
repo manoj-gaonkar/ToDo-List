a1fbb278e86aa4865403bdd470fd5a6a0f1b753f
manoj-gaonkar <manojgaonkar7648@gmail.com>
2022-09-06 11:04:19 +0530

    functionalities added

--- todo/views.py
+++ todo/views.py
@@ -4,12 +4,19 @@ from .models import Todo
 def index(request):
     if request.method == 'POST':
         new_todo = Todo( 
-            title= request.POST['title']
+            title= request.POST['title'],
+            description= request.POST['description']
         )
         new_todo.save()
 
         return redirect('/')
     return render(request, 'todo/index.html',{'todos': Todo.objects.all()})
+
+def login(request):
+    return render(request, 'todo/login.html')
+
+def signup(request):
+    return render(request, 'todo/signup.html')
 
 def delete(request,pk):
     delobj = Todo.objects.get(id=pk)
